4777714a6c4049b74be0d55c2efa06e0cffb15f3
Григорий <tydykovga@mail.ru>
2018-12-16 19:09:11 +0700

    Создает валидацию формы

--- js/main.js
+++ js/main.js
@@ -28,6 +28,7 @@ var TYPES = {
   BUNGALO: 'Бунгало',
   FLAT: 'Кваритра'
 };
+var ESC_KEYCODE = 27;
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
@@ -234,7 +235,7 @@ var isShowAds = function () {
 var hideAds = function (evt) {
   var card = document.querySelector('.map__card');
   var popupClose = document.querySelector('.popup__close');
-  if (evt.target.closest('.popup__close') || evt.keyCode === 27) {
+  if (evt.target.closest('.popup__close') || evt.keyCode === ESC_KEYCODE) {
     card.remove();
     popupClose.removeEventListener('click', hideAds);
     document.removeEventListener('keydown', hideAds);
@@ -249,3 +250,103 @@ var renderPins = function () {
 };
 
 pinMain.addEventListener('mouseup', renderPins);
+
+var numRooms = noticeForm.querySelector('#room_number');
+var capacity = noticeForm.querySelector('#capacity');
+var typeHousing = noticeForm.querySelector('#type');
+var price = noticeForm.querySelector('#price');
+var timein = noticeForm.querySelector('#timein');
+var timeout = noticeForm.querySelector('#timeout');
+
+var setPlaceholderAndMinPrice = function (minValue) {
+  price.value = '';
+  price.placeholder = minValue;
+  price.min = minValue;
+};
+
+var timingTypeAndPriceHousing = function () {
+  if (typeHousing.value === 'bungalo') {
+    setPlaceholderAndMinPrice(0);
+  } else if (typeHousing.value === 'flat') {
+    setPlaceholderAndMinPrice(1000);
+  } else if (typeHousing.value === 'house') {
+    setPlaceholderAndMinPrice(5000);
+  } else if (typeHousing.value === 'palace') {
+    setPlaceholderAndMinPrice(10000);
+  }
+};
+timingTypeAndPriceHousing();
+typeHousing.addEventListener('input', timingTypeAndPriceHousing);
+
+var disabledNumRooms = function () {
+  for (var i = 0; i < capacity.options.length; i++) {
+    capacity.options[i].setAttribute('disabled', '');
+  }
+};
+
+var includeNumRooms = function (index) {
+  capacity.options[index].removeAttribute('disabled');
+};
+
+var timingNumRoomsAndGuests = function () {
+  capacity.value = numRooms.value;
+  if (numRooms.value === '100') {
+    capacity.value = '0';
+  }
+  disabledNumRooms();
+  for (var j = 0; j < capacity.options.length; j++) {
+    if (capacity.value === '3' && j < 3) {
+      includeNumRooms(j);
+    } else if (capacity.value === '2' && j > 0 && j < 3) {
+      includeNumRooms(j);
+    } else if (capacity.value === '1' && j === 2) {
+      includeNumRooms(j);
+    } else if (capacity.value === '0' && j === 3) {
+      includeNumRooms(j);
+    }
+  }
+};
+
+timingNumRoomsAndGuests();
+numRooms.addEventListener('input', timingNumRoomsAndGuests);
+
+var setTimeinAndTimeout = function (time) {
+  timein.value = time;
+  timeout.value = time;
+};
+var timingTimeinAndTimeout = function (evt) {
+  if (evt.target.value === '12:00') {
+    setTimeinAndTimeout('12:00');
+  } else if (evt.target.value === '13:00') {
+    setTimeinAndTimeout('13:00');
+  } else if (evt.target.value === '14:00') {
+    setTimeinAndTimeout('14:00');
+  }
+};
+timein.addEventListener('input', timingTimeinAndTimeout);
+timeout.addEventListener('input', timingTimeinAndTimeout);
+
+
+var hideSuccessForm = function (evt) {
+  var success = document.querySelector('.success');
+
+  if (evt.keyCode === ESC_KEYCODE || evt.type === 'mousedown') {
+    success.remove();
+    document.removeEventListener('keydown', hideSuccessForm);
+    document.removeEventListener('mousedown', hideSuccessForm);
+  }
+};
+
+
+noticeForm.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  var success = document.querySelector('#success');
+  var successForm = success.content.cloneNode(true);
+  var main = document.querySelector('main');
+
+  main.appendChild(successForm);
+
+  document.addEventListener('keydown', hideSuccessForm);
+  document.addEventListener('mousedown', hideSuccessForm);
+});
+
